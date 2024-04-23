2d7414776b7c767af2c045ea3e2c650f763c9195
Raul Valdez <rd.valdez@outlook.com>
2024-04-23 14:22:51 -0500

    Added changes to the medventory app and connected it in the project settings.

--- medventory/models.py
+++ medventory/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Models for the vendor information
+class Vendor(models.Model):
+    name = models.CharField(max_length=64)
+    address = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    state = models.CharField(max_length=64)
+    zip = models.CharField(max_length=64)
+    phone = models.CharField(max_length=64)
+    fax = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+    website = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
