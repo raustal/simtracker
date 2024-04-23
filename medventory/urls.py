c45cc0024257069d3dbb3afab42eb32c022df836
Raul Valdez <rd.valdez@outlook.com>
2024-04-23 15:40:12 -0500

    Refactored index.html and added a details page.

--- medventory/urls.py
+++ medventory/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("vendor/", views.index, name="index"),
+    path("vendor/<int:vendor_id>", views.vendor_details, name="vendor_details"),
 ]
--- medventory/views.py
+++ medventory/views.py
@@ -4,4 +4,11 @@ from .models import Vendor
 # Create your views here.
 def index(request):
     vendors = Vendor.objects.all()
-    return render(request, "medventory/index.html", {"vendors": vendors})
+    return render(request, "medventory/vendor_list.html", {"vendors": vendors})
+
+def vendor_details(request, vendor_id):
+    try:
+        vendor = Vendor.objects.get(pk=vendor_id)
+    except Vendor.DoesNotExist:
+        return render(request, "medventory/404.html", {"title": "Vendor Not Found"})
+    return render(request, "medventory/vendor_details.html", {"vendor": vendor, "title": "Vendor Details"})
