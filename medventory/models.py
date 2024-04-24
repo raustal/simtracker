c8ca9ddc554738a46b3cdc0a81c95d97c84e1324
Raul Valdez <rd.valdez@outlook.com>
2024-04-23 21:22:47 -0500

    Added medication and unit models.

--- medventory/models.py
+++ medventory/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Models for the vendor information
 class Vendor(models.Model):
     name = models.CharField(max_length=64)
@@ -14,3 +15,24 @@ class Vendor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.name}"
+
+
+class Medication(models.Model):
+    generic_name = models.CharField(max_length=64)
+    trade_name = models.CharField(max_length=64)
+    dose = models.FloatField()
+    unit = models.ForeignKey("Unit", on_delete=models.CASCADE)
+    vendor = models.ForeignKey("Vendor", on_delete=models.CASCADE)
+    item_number = models.CharField(max_length=64)
+    barcode = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"{self.generic_name}"
+
+
+class Unit(models.Model):
+    unit = models.CharField(max_length=64)
+    unit_abbreviation = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"{self.unit}"
