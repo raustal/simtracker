fd6336ad9514e27561c6d03daf226d9f514e24f2
Raul Valdez <rd.valdez@outlook.com>
2024-04-23 21:26:36 -0500

    Added migrations files and models to admin file.

--- medventory/admin.py
+++ medventory/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Vendor
+from .models import Vendor, Medication, Unit
 
 
 # Register your models here.
 admin.site.register(Vendor)
+admin.site.register(Medication)
+admin.site.register(Unit)
